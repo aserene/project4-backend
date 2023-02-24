4d6945ef358cec0756281ecab870d1da18e26862
Athena Serene <aserenehoosang@gmail.com>
2023-02-23 18:16:43 -0800

    migrated model

--- backend/settings.py
+++ backend/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'wares.apps.MercantileConfig',
+    'wares.apps.WaresConfig',
     'rest_framework'
 ]
 
--- wares/apps.py
+++ wares/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MercantileConfig(AppConfig):
+class WaresConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mercantile'
+    name = 'wares'
