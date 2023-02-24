55f5e86cc60cebed6d8b8dac14e88dfc97ef2d07
Athena Serene <aserenehoosang@gmail.com>
2023-02-23 18:35:27 -0800

    views.py done

--- wares/views.py
+++ wares/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Ware
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import WareSerializer
 # Create your views here.
+class WareViewSet (viewsets.ModelViewSet):
+    queryset=Ware.objects.all()
+    serializer_class=WareSerializer
+    permission_classes=[permissions.AllowAny]
